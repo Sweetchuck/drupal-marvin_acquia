805c29cc396d38742c4d2c6f03d38ec40a2dd1fb
Dávid Andor <david-andor@kozpontiagy.hu>
2019-08-06 23:50:19 +0200

    Artifact with GitCloneAndClean

--- Commands/marvin_acquia/ArtifactBuildAcquiaCommands.php
+++ Commands/marvin_acquia/ArtifactBuildAcquiaCommands.php
@@ -7,13 +7,17 @@ namespace Drush\Commands\marvin_acquia;
 use Drupal\marvin\Robo\VersionNumberTaskLoader;
 use Drush\Commands\marvin_product\ArtifactBuildProductCommandsBase;
 use Robo\Collection\CollectionBuilder;
+use Robo\State\Data as RoboStateData;
+use Sweetchuck\Robo\Git\GitComboTaskLoader;
 use Sweetchuck\Robo\Git\GitTaskLoader;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Finder\Finder;
 
 class ArtifactBuildAcquiaCommands extends ArtifactBuildProductCommandsBase {
 
   use VersionNumberTaskLoader;
   use GitTaskLoader;
+  use GitComboTaskLoader;
 
   /**
    * {@inheritdoc}
@@ -76,6 +80,91 @@ class ArtifactBuildAcquiaCommands extends ArtifactBuildProductCommandsBase {
     $this->versionPartToBump = $input->getOption('version-bump');
 
     return $this->getBuildSteps();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getBuildSteps(): array {
+    $steps = parent::getBuildSteps();
+
+    $weight = $steps['prepareDirectory.marvin']['weight'] ?? 0;
+    $steps['prepareDirectory.marvin_acquia'] = [
+      'task' => $this->getTaskGitCloneAndClean(),
+      'weight' => $weight + 1,
+    ];
+
+    $weight = $steps['copyFilesCollect.marvin']['weight'] ?? 0;
+    $steps['copyFilesCollect.marvin_acquia'] = [
+      'task' => $this->getTaskCollectFilesAcquia(),
+      'weight' => $weight + 1,
+    ];
+
+    return $steps;
+  }
+
+  /**
+   * @return \Closure|\Robo\Contract\TaskInterface
+   */
+  protected function getTaskGitCloneAndClean() {
+    $config = $this->getConfig();
+    $projectId = (string) $config->get('marvin.acquia.projectId');
+    if (!$projectId) {
+      return function (): int {
+        $logger = $this->getLogger();
+        $logContext = [
+          'taskName' => 'Git clone and clean',
+        ];
+
+        $logger->notice(
+          '{taskName} - skipped because of the lack of marvin.acquia.projectId',
+          $logContext
+        );
+
+        return 0;
+      };
+    }
+
+    $remoteName = (string) $config->get('marvin.acquia.remoteName');
+    $remoteUrl = (string) $config->get('marvin.acquia.remoteUrl');
+    $remoteBranch = (string) $config->get('marvin.acquia.remoteBranch');
+    $localBranch = (string) $config->get('marvin.acquia.localBranch');
+
+    return $this
+      ->taskGitCloneAndClean()
+      ->setSrcDir($this->srcDir)
+      ->setRemoteName($remoteName)
+      ->setRemoteUrl($remoteUrl)
+      ->setRemoteBranch($remoteBranch)
+      ->setLocalBranch($localBranch)
+      ->deferTaskConfiguration('setWorkingDirectory', 'buildDir');
+  }
+
+  /**
+   * @return \Closure|\Robo\Contract\TaskInterface
+   */
+  protected function getTaskCollectFilesAcquia() {
+    return function (RoboStateData $data): int {
+      if ($this->fs->exists("{$this->srcDir}/hooks/")) {
+        $artifactDir = $this->getConfig()->get('marvin.artifactDir', 'artifacts');
+        $artifactDirSafe = preg_quote($artifactDir, '@');
+
+        /** @var \Drupal\marvin\ComposerInfo $composerInfo */
+        $composerInfo = $data['composerInfo'];
+        $docroot = $composerInfo->getDrupalRootDir();
+        $docrootSafe = preg_quote($docroot, '@');
+
+        $data['files'][] = (new Finder())
+          ->in($this->srcDir)
+          ->path('hooks')
+          ->notPath("@^{$artifactDirSafe}@")
+          ->notPath("@^{$docrootSafe}/sites/simpletest/@")
+          ->ignoreDotFiles(FALSE)
+          ->files();
+      }
+
+      return 0;
+    };
   }
 
 }
