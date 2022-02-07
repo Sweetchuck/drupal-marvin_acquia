fdfcfb5be3ff98316a647942bb04454f5151d521
Dávid Andor <david-andor@kozpontiagy.hu>
2022-02-07 19:02:25 +0100

    Upgrade to drupal/core:9.3.x drush/drush:11.x consolidation/robo:3.x

---
+++ tests/src/Unit/Commands/marvin_acquia/TestBase.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\marvin_acquia\Unit\Commands\marvin_acquia;
+
+use PHPUnit\Framework\TestCase;
+
+abstract class TestBase extends TestCase {
+}
